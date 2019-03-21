3f6c3dd82ec5744d6a8f99bec8dc73a0e11ab0c7
John Niedfeldt <43926285+jcniedfeldt@users.noreply.github.com>
2019-03-20 21:30:12 -0700

    Revert "initial files"

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Axios from "axios";
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books" component={Books} />
-          <Route exact path="/books/:id" component={Detail} />
-          <Route component={NoMatch} />
-        </Switch>
+class App extends Component {
+state={
+  success:false
+}
+componentDidMount(){
+  Axios.get("/api").then(({data})=>{
+    this.setState({success:data.success})
+  })
+}
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
-    </Router>
-  );
+    );
+  }
 }
 
 export default App;
