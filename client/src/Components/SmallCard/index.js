6e0ae5f5ca77c0600a49eec9e109d8c0c7a7522f
Blue Buffalo <anettagoldsher@gmail.com>
2019-03-27 15:38:12 -0700

    minor additions

--- client/src/Components/SmallCard/index.js
+++ client/src/Components/SmallCard/index.js
@@ -9,7 +9,7 @@ function SmallCard(props) {
               <img src={props.image} alt={props.title} style={{width:"100%"}}/>
               <div className="caption text-center">
                 <h5 className="card-title">{props.title}</h5>
-                <p className="card-text">${props.price}</p>
+                <p className="card-text">${props.price}/day</p>
               </div>
              </a>
           </div>
