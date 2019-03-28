3fcafc404e0784d9051626be4a1f14cc66ac7ee9
Link <username@users.noreply.github.com>
2019-03-27 21:25:03 -0700

    merge

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -14,7 +14,7 @@ class Navbar extends Component {
         return (
             <nav className="navbar navbar-light bg-light">
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span></button>
+                <span className="navbar-toggler-icon"></span></button>
                 <Input
                     placeholder="Search..."
                 />
