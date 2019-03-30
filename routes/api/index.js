13f997c58e3941d7f075beb31c9e5ced81f5840d
John Niedfeldt <jc@pop-os.localdomain>
2019-03-30 02:29:58 -0700

    Passport backend functional

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const postRoutes = require("./posts");
+const userRoutes = require("./users");
 
 // Post routes
 router.use("/posts", postRoutes);
+router.use("/users", userRoutes);
+// router.use("/login", userRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,28 +1,74 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
 const PORT = process.env.PORT || 3001;
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const errorHandler = require('errorhandler');
+
+//Configure mongoose's promise to global promise
+mongoose.promise = global.Promise;
+
+//Configure isProduction variable
+const isProduction = process.env.NODE_ENV === 'production';
+
 const app = express();
 
-
 // Define middleware here
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (isProduction) {
+  // app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 }
 // Add routes, both API and view
-app.use(routes);
+
 
 // Define API routes here
 // app.get("/api",(res,req)=>{
 //   res.json({success:true})
 // })
 
+if(!isProduction) {
+  app.use(errorHandler());
+}
+
 // Connecting to Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/theminimalist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/theminimalist", { useNewUrlParser: true });
+mongoose.set('debug', true);
+
+
+ 
+//Error handlers & middlewares
+if(!isProduction) {
+  app.use((err, req, res,next) => {
+    // console.log(req)
+    console.log(res)
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+// Passport model import
+require('./models/Users');
+require('./config/passport');
+const routes = require("./routes");
+app.use(routes);
+app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
 // Send every other request to the React app
 // Define any API routes before this runs
@@ -31,6 +77,6 @@ app.get("*", (req, res) => {
 });
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
