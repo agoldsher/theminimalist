a6fd9dff8af3201a4d396134b1c10a75fb97b946
Blue Buffalo <anettagoldsher@gmail.com>
2019-03-29 17:35:45 -0700

    hamburger working

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -4,17 +4,42 @@ import { Input, FormBtn } from "../AddForm";
 import { Link } from "react-router-dom";
 
 class Navbar extends Component {
-
+ state = {
+    categories:["electronics",
+    "appliances",
+    "clothing",
+    "household",
+    "sports/outdoors",
+    "movies and games",
+    "space",
+    "machinary",
+    "tools"]
+    };
     change = (x) => {
         x.classList.toggle("change");
     }
-
-
+   
+ 
     render() {
         return (
             <nav className="navbar navbar-light bg-light">
+            <div className="pos-f-t">
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span></button>
+                <div className="collapse" id="navbarToggleExternalContent">
+                    <div className="bg-light p-4">
+                        <ul className="text-dark h5">
+                        {/* {this.state.categories.forEach(category => {
+                           return category;
+                        })} */}
+                        <li>electronics</li>
+                        <li>appliances</li>
+                        <li>clothing</li>
+                        </ul>
+                     
+                    </div>
+                </div>
+            </div>
                 <Input
                     placeholder="Search..."
                 />
