42d7d3ef6b15eeb3301e5e59f0a21740723a95be
John Niedfeldt <jc@pop-os.localdomain>
2019-04-03 00:07:42 -0700

    Added auth and redux to frontend. Dashboard not yet fully functional. Trying to get Main page to work properly.

--- client/src/App.js
+++ client/src/App.js
@@ -1,74 +1,81 @@
-// import React, { Component } from "react";
-
-// import "./App.css";
-// import Axios from "axios";
-// import CardRender from "./Components/CardRender"
-
-// class App extends Component {
-// state={
-//   success:false
-// }
-// componentDidMount(){
-//   Axios.get("/api").then(({data})=>{
-//     this.setState({success:data.success})
-//   })
-// }
-//   render() {
-//     return (
-//     <CardRender></CardRender>
-//     );
-//   }
-// }
-
-// export default App;
-
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./Pages/Main";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
 // import Category from "./Pages/Category";
 // import Search from "./Pages/Search";
 import NewPost from "./Pages/NewPost";
 import NoMatch from "./Pages/NoMatch";
 import Detail from "./Pages/Detail";
 import Navbar from "./Components/Navbar";
+import Register from "./Pages/Auth/Register";
+import Login from "./Pages/Auth/Login";
+// import Landing from "./Pages/Landing";
 import API from './utils/API'
+import PrivateRoute from "./Pages/private-route/PrivateRoute";
+import Dashboard from "./Pages/dashboard/Dashboard";
 
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+// Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 
 class App extends React.Component {
   state = {
-    cards:[],
+    cards: [],
     category: "",
     search: ""
   }
   loadPopPosts = () => {
     API.getPopPosts()
-       .then(res =>{
-         this.setState({ cards: res.data });
-         console.log(res.data)
-       }
-       )
-       .catch(err => console.log(err));
-   };
-  handleCategoryChange = (category)=>{
-    if (category==="All"){
+      .then(res => {
+        this.setState({ cards: res.data });
+        console.log(res.data)
+      }
+      )
+      .catch(err => console.log(err));
+  };
+  handleCategoryChange = (category) => {
+    if (category === "All") {
       this.loadPopPosts();
-    }else{
-    API.getCategoryPosts(category)
-        .then(res =>{
+    } else {
+      API.getCategoryPosts(category)
+        .then(res => {
           this.setState({ cards: res.data, category });
           console.log(res.data)
         }
         )
         .catch(err => console.log(err));
-  }}
-  handleSearch = (search)=>{
+    }
+  }
+  handleSearch = (search) => {
     API.search(search)
-        .then(res =>{
-          this.setState({ cards: res.data, search});
-          console.log(res.data)
-        }
-        )
-        .catch(err => console.log(err));
+      .then(res => {
+        this.setState({ cards: res.data, search });
+        console.log(res.data)
+      }
+      )
+      .catch(err => console.log(err));
   }
 
 
@@ -76,21 +83,28 @@ class App extends React.Component {
     this.loadPopPosts();
   }
 
-  render(){
+  render() {
     return (
-      <Router>
-        <div>
-          <Navbar handleCategoryChange={this.handleCategoryChange} handleSearch={this.handleSearch} />
-          <Switch>
-            <Route exact path="/" render={(props) => <Main {...props} cards={this.state.cards} />} />
-            <Route exact path="/newpost" component={NewPost} />
-            {/* <Route exact path="/category/:category" component={Category} />
+      <Provider store={store}>
+        <Router>
+          <div>
+            <Navbar handleCategoryChange={this.handleCategoryChange} handleSearch={this.handleSearch} />
+            <Switch>
+              <Route exact path="/" render={(props) => <Main {...props} cards={this.state.cards} />} />
+              {/* <Route exact path="/" component={Main} cards={this.state.cards} /> */}
+              {/* <Route exact path="/land" component={Landing} /> */}
+              <PrivateRoute exact path="/dash" component={Dashboard} />
+              <Route exact path="/newpost" component={NewPost} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              {/* <Route exact path="/category/:category" component={Category} />
             <Route exact path="/search/:search" component={Search} /> */}
-            <Route exact path="/:id" component={Detail} />
-            <Route component={NoMatch} />
-          </Switch>
-        </div>
-      </Router>
+              <Route exact path="/:id" component={Detail} />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require("dotenv").config()
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
