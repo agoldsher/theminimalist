fcb8561c7e8dcc255ffc0a8f167d09c79a1ec026
John Niedfeldt <jc@pop-os.localdomain>
2019-04-10 19:59:37 -0700

    Added protection to all routes except for login and register routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,16 @@ const router = require("express").Router();
 const postRoutes = require("./posts");
 const userRoutes = require("./user");
 const zipCodeRoutes = require("./zipCode");
+// module.exports = function () {
+    module.exports = function (passport) {
 
-// Post routes
-router.use("/posts", postRoutes);
-router.use("/users", userRoutes);
-router.use("/zipCode", zipCodeRoutes);
+    // Post routes
+    // router.use("/posts", postRoutes);
+    router.use("/posts", passport.authenticate('jwt'), postRoutes);
+    router.use("/users", userRoutes);
+    // router.use("/zipCode", zipCodeRoutes);
+    router.use("/zipCode", passport.authenticate('jwt'), zipCodeRoutes);
 
-module.exports = router;
+
+    return router;
+}
