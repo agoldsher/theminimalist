21e9019884355a4c7e5130175b5aafa7a365ba9e
John Niedfeldt <jc@pop-os.localdomain>
2019-04-11 13:08:37 -0700

    Test1

--- client/src/reducers/authReducers.js
+++ client/src/reducers/authReducers.js
@@ -21,6 +21,7 @@ export default function (state = initialState, action) {
                 ...state,
                 loading: true
             };
+            
         default:
             return state;
     }
