e067a6140122546c520b46cefbbb0d743406f1dc
John Niedfeldt <43926285+jcniedfeldt@users.noreply.github.com>
2019-04-11 13:23:23 -0700

    Revert "Authentication for api routes added. Changed SCSS imports for materialize to CSS."

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,16 +2,10 @@ const router = require("express").Router();
 const postRoutes = require("./posts");
 const userRoutes = require("./user");
 const zipCodeRoutes = require("./zipCode");
-// module.exports = function () {
-    module.exports = function (passport) {
 
-    // Post routes
-    // router.use("/posts", postRoutes);
-    router.use("/posts", passport.authenticate('jwt'), postRoutes);
-    router.use("/users", userRoutes);
-    // router.use("/zipCode", zipCodeRoutes);
-    router.use("/zipCode", passport.authenticate('jwt'), zipCodeRoutes);
+// Post routes
+router.use("/posts", postRoutes);
+router.use("/users", userRoutes);
+router.use("/zipCode", zipCodeRoutes);
 
-
-    return router;
-}
+module.exports = router;
