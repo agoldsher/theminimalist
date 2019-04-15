24e395bea5235ab549d74e4052bdfb080234afff
Link <username@users.noreply.github.com>
2019-04-15 12:41:10 -0700

    try2

--- client/src/Pages/Message.js
+++ client/src/Pages/Message.js
@@ -19,7 +19,7 @@ class Message extends React.Component {
             postCity: "",
             loggedInName: ""
         };
-        this.socket = io("mongo ds121026.mlab.com:21026/heroku_w8pp3q6n -u <heroku_w8pp3q6n>");
+        this.socket = io("mongo ds121026.mlab.com:21026/heroku_w8pp3q6n -u <heroku_w8pp3q6n> -p <theminimalist2019!>");
     };
 
     componentDidMount() {
